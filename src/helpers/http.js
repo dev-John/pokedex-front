f7e48f5fdb033f57231f45cacdcc95d96aa26274
Jonatas <jonatasoliveira1234@gmail.com>
2021-03-21 11:11:32 -0300

    New env var

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -5,7 +5,7 @@ const codes = [413, 401, 400, 403];
 const validateStatus = (status) => (status >= 200 && status < 300) || codes.includes(status);
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: process.env.REACT_APP_API_URL,
   validateStatus,
   withCredentials: true,
 });
