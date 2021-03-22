da23c3abff5ba4e216f3bc012de8d848694a6337
Jonatas Oliveira <jonatas.o.silva@kroton.com.br>
2021-03-22 14:46:03 -0300

    Remove curr user from storage on quit

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-
+import Grid from '@material-ui/core/Grid';
 import PokemonList from './containers/PokemonList';
 import PokemonForm from './containers/Pokemon';
 import Login from './containers/Login';
 import Signup from './containers/Signup';
 import QRCodeVerification from './containers/QRCodeVerification';
 import { isAuthenticated } from './helpers/auth';
+import { ROUTES } from './constants';
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route 
     { ...rest }
     render={props => isAuthenticated() ? (
-      <Component { ...props } />
+      <>
+        <Component { ...props } />
+        <Grid container justify="center">
+          <a onClick={() =>  localStorage.removeItem('currentUser')} href={ROUTES.LOGIN}>
+            <small>Sair</small>
+          </a>
+        </Grid>
+      </>
     ): (
       <Redirect to={{ pathname: '/', state: { from: props.location } }} />
     )
